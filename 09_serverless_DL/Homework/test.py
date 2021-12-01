4ae7405fe76b64927f8c117529d1762f98dca73e
sanghvirajit19 <sanghvirajit19@gmail.com>
2021-12-01 09:08:44 +0100

    deep_learning_nn

--- 09_serverless_DL/Homework/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-#url = 'https://nc2yw1ff25.execute-api.eu-central-1.amazonaws.com/test/predict'
-
-data = {'url': 'https://upload.wikimedia.org/wikipedia/commons/1/18/Vombatus_ursinus_-Maria_Island_National_Park.jpg'}
-
-result = requests.post(url, json=data).json()
-print(result)
