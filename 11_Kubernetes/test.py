27b4b7ebaff0e79a85029ac8632b5b8593c3cee3
Your Name <you@example.com>
2022-01-03 20:14:16 +0530

    kubernetes

---
+++ 11_Kubernetes/test.py
@@ -0,0 +1,12 @@
+import requests
+
+#url = 'http://localhost:9696/predict'
+url = 'http://localhost:8080/predict'
+
+data = {
+    "url": "http://bit.ly/mlbookcamp-pants"
+}
+
+results = requests.post(url, json=data).json()
+
+print(results)
